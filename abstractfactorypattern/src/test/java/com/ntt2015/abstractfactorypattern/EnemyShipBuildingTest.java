40b58d7e1fc16f5f6d14c8f8d78a6dbe7d99f50a
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-02-10 01:19:39 -0500

    20220210-1 added log4j-1.2.17, junit-4.13, stub test suite

---
+++ abstractfactorypattern/src/test/java/com/ntt2015/abstractfactorypattern/EnemyShipBuildingTest.java
@@ -0,0 +1,17 @@
+package com.ntt2015.abstractfactorypattern;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class EnemyShipBuildingTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
