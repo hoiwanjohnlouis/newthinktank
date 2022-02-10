5c132d72a12ae79ac9eb4b5a3727ca0fec4533e0
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-02-10 01:47:48 -0500

    20220210-1 added log4j-1.2.17, junit-4.13, stub test suite

---
+++ observerpattern/src/test/java/com/ntt2015/observerpattern/StockObserverTest.java
@@ -0,0 +1,13 @@
+package com.ntt2015.observerpattern;
+
+import junit.framework.TestCase;
+
+public class StockObserverTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+}
